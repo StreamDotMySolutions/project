673a59c5b3427076594da81e703ccca4fa2c383b
StreamDotMy <streamdotmysolutions@gmail.com>
2024-03-31 15:01:04 +0800

    Lesson03 - start

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom" // Routing
 
 /**Elements **/
 import Home from './resources/Home'
 import Error from './resources/Error'
 import AboutUs from './resources/AboutUs'
+import Index from './resources/Index'
 
 // Component
 export default function App() {
@@ -14,12 +15,13 @@ export default function App() {
       <BrowserRouter basename="/"> 
         <Routes>     
           <Route path="*" element={<Error />} />       
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Index />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about-us" element={<AboutUs />} />
         </Routes>
       </BrowserRouter>
   );
 
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
